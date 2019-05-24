3c9dd840291e05fcd0b22f5fd35a97ddeb98a6fc
Yatheeshaiah <176501@NTTDATA.COM>
2019-05-24 12:30:03 +0530

    second is added

---
+++ second/Second1.java
@@ -0,0 +1,12 @@
+package second;
+
+public class Second1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<5;i++)
+			System.out.println("hellooo");
+
+	}
+
+}
